5df426e4ba1cd2e2fcd16f76248c38482a73adc9
Ajeng Gusti Putri Cempaka Sari <111729726+ajenggusti@users.noreply.github.com>
2023-12-11 11:35:14 +0700

    streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -30,12 +30,6 @@ def predict_nsp(input_data):
 
     return predictions
 
-# Function to evaluate model accuracy
-def evaluate_accuracy(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    return accuracy
 
 # Streamlit web app
 def main():
@@ -52,16 +46,6 @@ def main():
         input_data = [user_input[column] for column in columns_order]
         prediction = predict_nsp(input_data)
         st.write(f"Predicted NSP: {prediction[0]}")
-
-    # Evaluate model accuracy
-    data = pd.read_excel("CTG_resampled.xlsx")  # ini diisi dengan data faktual
-    features_to_exclude = ['DR', 'DS', 'Nzeros', 'FileName', 'Date', 'SegFile','A','B','C','D','E','AD','DE','LD','FS','SUSP','CLASS','b','e'] # ini kolom yang dikecualikan, ada 5
-    X = data.drop(['NSP'] + features_to_exclude, axis=1)  # pengecualian kolom target dan kolom fitur yang tidak diinginkan, jadi disini ada 6 kolom yang dihindari
-    y = data['NSP']  # Menggunakan kolom 'NSP' sebagai target
-
-
-    accuracy = evaluate_accuracy(X, y)
-    st.write(f"Model Accuracy: {accuracy:.2%}")
 
 if __name__ == "__main__":
     main()
